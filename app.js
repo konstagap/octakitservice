27b3393a9773b66afbf8f979d5cfb88bf495694f
ka <konstagpov123@gmail.com>
2022-10-16 19:56:39 -0700

    adding cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const router = require('./router');
 const errorHandler = require('./middleware/error-middleware');
 
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
